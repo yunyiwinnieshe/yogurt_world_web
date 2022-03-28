46bd59374a39d63518a785e4a43476653e797f3e
yunyiwinnieshe <pineapplesyy@gmail.com>
2022-03-28 15:19:05 -0700

    first commit

---
+++ scripts/menu.js
@@ -0,0 +1,23 @@
+// function toggleNavBar() {
+//     let navButton = document.getElementById("hamburgerIcon2"); 
+
+//     if (navButton.className == "fa fa-bars menu-toggle") {
+//         navButton.className = "fa fa-close"; 
+//     }
+//     else if (navButton.className == "fa fa-close") {
+//         navButton.className = "fa fa-bars menu-toggle"; 
+//     }
+// }   
+
+function toggleNavBar() {
+    let navButton = document.getElementById("hamburgerIcon2"); 
+    let toggle = document.getElementById("toggle"); 
+
+    if (!toggle.checked) {
+        navButton.className = "fa fa-close"; 
+    }
+    else {
+        navButton.className = "fa fa-bars menu-toggle"; 
+    }
+
+}  
